c33743b91601882c44ccbfac1913f472e5f54562
Avinash1813111 <Avinashpandey1813111@gmail.com>
2025-11-10 05:55:49 +0530

    Hoisting

--- JS-Practice/Hoisting.js
+++ JS-Practice/Hoisting.js
@@ -1,1 +1,17 @@
-console.log("Hoisting")
+// Hoisting is a phenomena by which we can access the functions and variables before we intializing it.
+
+console.log(a)  // undefined
+var a=10;
+console.log(a); //10
+
+
+//Hoisting works diffrently in LET and const case
+//hoisting can be done only in temporal deadzone
+
+
+console.log(b) //refrence errror
+console.log(c) // refrence error
+let b=20;
+const c=30;
+console.log(b)  // 20
+console.log(c)  //30
